47e3a2edd2fb8d7841e8cbbbc54ebb58f0bab837
tiago israel <tisilva@latam.stefanini.com>
2019-11-11 00:15:56 -0300

    add logo

--- src/layouts/header/Index.jsx
+++ src/layouts/header/Index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./styles";
-import Logo from "../../assets/icons/logo.png";
+import Logo from "../../assets/icons/logo.svg";
 
 export default () => (
   <Header>
